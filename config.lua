d540a3bdbb6509372d4b6cefa7f07f76057e4d20
Ultra Scripts <88847062+Butterchilly@users.noreply.github.com>
2023-08-01 19:08:38 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.emoteName = "dancethriller" --- U can change ur emote here also or with /setemote command
+Config.emoteDuration = 1000
+Config.exportName = "rpemotes" -- You have to change to name here according to the name of your emote script name. and change dependecy also in fxmanifest.lua
+
+Config.zones = {
+    {center = vector3(197.29, 383.97, 107.84), radius = 5.0},
+    {center = vector3(215.00, 365.00, 105.00), radius = 5.0},
+    -- Add more zones here
+}

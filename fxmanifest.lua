84f6cb1081bfea63f8c8c62a7914e54e39211450
Ultra Scripts <88847062+Butterchilly@users.noreply.github.com>
2023-08-01 19:08:42 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Sunny'
+lua54 "yes"
+
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+server_script 'server.lua'
+
+dependency 'rpemotes'
+
